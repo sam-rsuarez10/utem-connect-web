12d6b55a4aea3cc6479976f5118724ccca4e0b8d
sam-rsuarez10 <suarezzcode2001@gmail.com>
2023-10-14 17:56:21 -0300

    add welcome description for loggen in users

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,6 +19,15 @@ const user = authStore.user;
           <p>Welcome, {{ user }}!</p>
         </div>
       </div>
+      <div class="welcome-container">
+        <i class='bx bxs-smile bx-lg' style='color:#62bd62'  ></i>
+        <p class="welcome">
+          ¡Hola {{ user }}! Conéctate, busca amigos, 
+          envía solicitudes, chatea en privado y comparte tus pensamientos 
+          e ideas en un espacio diseñado 
+          para la comunidad universitaria de la UTEM.
+        </p>
+      </div>
     </main>
     <Footer />
   </div>
@@ -36,6 +45,24 @@ main {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+/* Style for the welcome div */
+.welcome-container {
+  position: absolute;
+  left: 0;
+  bottom: 5rem;
+  width: 17rem;
+  background-color: #4B8C8F;
+  padding: 0.75rem;
+  border-radius: 5%;
+}
+
+.welcome {
+  margin: 0;
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 0.85rem;
+  color: white;
 }
 
 </style>
